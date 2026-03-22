a2d1a32d2f3c15092df05de5f1a46b00d1f03dd9
ggmusique <geohelene@msn.com>
2026-03-22 16:53:56 +0100

    Update Supabase client configuration
    Replaced environment variables with hardcoded Supabase URL and anon key.

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = 'https://mofyzotmnnjqmmudjppi.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vZnl6b3Rtbm5qcW1tdWRqcHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwOTgzOTUsImV4cCI6MjA4OTY3NDM5NX0.3rXz33i6wf0cBQIg4edz43mwTdgngdvoDZwoZgYEHD0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
