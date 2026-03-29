3d2b8ff56964caf1454ec2dbc7427ab3a7dc83df
ggmusique <geohelene@msn.com>
2026-03-29 14:08:33 +0200

    Update import from engine to engineV2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useCallback, useEffect } from 'react';
-import { optimise } from './engine';
+import { optimise } from './engineV2';
 import { I18N, useLang } from './i18n';
 import { supabase, saveProject, loadProject, signOut } from './supabase';
 import ProjectForm from './components/ProjectForm';
